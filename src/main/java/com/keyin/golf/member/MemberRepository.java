f9de0ce66d17044084a90733309252740db47402
Maz Radwan <maz.radwan@keyin.com>
2024-11-19 18:55:55 -0330

    Initial commit

---
+++ src/main/java/com/keyin/golf/member/MemberRepository.java
@@ -0,0 +1,13 @@
+// src/main/java/com/keyin/golf/member/MemberRepository.java
+package com.keyin.golf.member;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.time.LocalDate;
+import java.util.List;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    List<Member> findByMemberNameContainingIgnoreCase(String name);
+    List<Member> findByMemberPhone(String phone);
+    List<Member> findByStartDate(LocalDate startDate);
+}
---
+++ src/main/java/com/keyin/golf/member/MemberService.java
@@ -0,0 +1,44 @@
+
+package com.keyin.golf.member;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MemberService {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    public List<Member> getAllMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> getMemberById(Long id) {
+        return memberRepository.findById(id);
+    }
+
+    public Member saveMember(Member member) {
+        return memberRepository.save(member);
+    }
+
+    public void deleteMember(Long id) {
+        memberRepository.deleteById(id);
+    }
+
+    // Search methods
+    public List<Member> searchByName(String name) {
+        return memberRepository.findByMemberNameContainingIgnoreCase(name);
+    }
+
+    public List<Member> searchByPhone(String phone) {
+        return memberRepository.findByMemberPhone(phone);
+    }
+
+    public List<Member> searchByStartDate(LocalDate startDate) {
+        return memberRepository.findByStartDate(startDate);
+    }
+}
